eb9998512955af1569a0c5d23d711bee029af958
Gagan CG <64187206+changu7007@users.noreply.github.com>
2023-04-04 21:37:38 +0530

    updated

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ import orderRoutes from "./routes/orderRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import bodyParser from "body-parser";
-import path from "path";
+import path from "path
+import {fileURLToPath} from 'url;
 
 //config env
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodulefix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
